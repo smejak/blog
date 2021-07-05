46fa206a18484b8417b5bfa727d1a0867bd3a2a5
Jakub Smekal <jakub.smekal@gmail.com>
2021-07-05 21:18:13 +0100

    Newsletter form

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,8 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        {new Date().getFullYear()}, Jakub Smekal
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
     </div>
   )
