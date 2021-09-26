df69d73e170772129e9199a3615b80408387a6bd
Jakub Smekal <jakub.smekal@gmail.com>
2021-09-26 13:31:52 +0100

    changed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,7 +119,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
